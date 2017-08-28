2bf461318a682e4ed32591e841572368ded9ce27
Rachid Groeneveld <rachidgroeneveld@hotmail.nl>
2017-08-28 12:41:58 +0200

    Initial version
    Test version ALPHA

--- install.sh
+++ install.sh
@@ -7,19 +7,23 @@ if [ ! -f $INSTALLING ]; then
 	touch $INSTALLING
 
 	# Download LMS 7.9.0
-	mkdir -p /home/volumio/logitechmediaserver
-	wget -O /home/volumio/logitechmediaserver http://downloads.slimdevices.com/LogitechMediaServer_v7.9.0/logitechmediaserver_7.9.0_arm.deb
+	echo "Downloading installation package..."
+	mkdir /home/volumio/logitechmediaserver
+	wget -O /home/volumio/logitechmediaserver/logitechmediaserver_7.9.0_arm.deb http://downloads.slimdevices.com/LogitechMediaServer_v7.9.0/logitechmediaserver_7.9.0_arm.deb
 	
 	# Install package and dependencies
+	echo "Installing downloaded package"
 	for f in /home/volumio/logitechmediaserver/logitechmediaserver*.deb; do dpkg -i "$f"; done
 	apt-get -f install
-
+	
+	# These directories still use the old name; probably legacy code
+	echo "Fixing directory rights"
 	mkdir /var/lib/squeezeboxserver
-	chown volumio:volumio /var/lib/squeezeboxserver
+	chown -R volumio:volumio /var/lib/squeezeboxserver
 
 	# Add the systemd unit
-	rm /etc/systemd/system/squeezeboxserver.service	
-	wget -O /etc/systemd/system/squeezeboxserver.service https://raw.githubusercontent.com/Saiyato/volumio-lms-plugin/master/units/squeezeboxserver.service
+	rm /etc/systemd/system/logitechmediaserver.service	
+	wget -O /etc/systemd/system/logitechmediaserver.service https://raw.githubusercontent.com/Saiyato/volumio-lms-plugin/master/units/logitechmediaserver.service
 	
 	rm $INSTALLING
 
