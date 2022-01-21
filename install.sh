d1b97da13e213a30b56b0d2cd80e3550fefa0d61
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2022-01-21 23:00:21 +0100

    Refactor the installation script
    Remove old fixes, that might not even be relevant anymore.
    
    Script now tries to fetch the latest version, always.

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ if [ ! -f $INSTALLING ]; then
 
 	touch $INSTALLING
 	
-	cpu=$(lscpu | awk 'FNR == 1 {print $2}')
-	echo "cpu: " $cpu
+	arch=$(arch)
+	echo "Detected architecture: " $arch
 
 	if [ ! -f /usr/sbin/squeezeboxserver ] || [ $1 =  "force" ];
 	then
@@ -22,13 +22,19 @@ if [ ! -f $INSTALLING ]; then
 			rm /home/volumio/logitechmediaserver/logitechmediaserver*.deb
 		fi
 		
-		if [ $cpu = "armv6l" ] || [ $cpu = "armv7l" ];
+		BASE_URL="http://downloads.slimdevices.com/"
+		LATEST=$(curl -s $BASE_URL | grep LogitechMediaServer_v | cut -d' ' -f2 | tail -n 1 | cut -d'"' -f2)
+		FILE=$(curl -s $BASE_URL$LATEST | grep arm | grep deb | cut -d'"' -f2)
+		
+		if [ $cpu = "armv6l" ] || [ $cpu = "armv7l" ] || [ $cpu = "armv8l" ]
 		then
-			wget -O /home/volumio/logitechmediaserver/logitechmediaserver_7.9.1_arm.deb http://downloads.slimdevices.com/LogitechMediaServer_v7.9.1/logitechmediaserver_7.9.1_arm.deb
-			#wget -O /home/volumio/logitechmediaserver/logitechmediaserver_7.9.2_arm.deb http://downloads-origin.slimdevices.com/nightly/7.9/sc/035da986803908908cb46501efbca6c4d6e93db2/logitechmediaserver_7.9.2~1543258616_arm.deb
-		else
-			wget -O /home/volumio/logitechmediaserver/logitechmediaserver_7.9.1._all.deb http://downloads.slimdevices.com/LogitechMediaServer_v7.9.1/logitechmediaserver_7.9.1_all.deb
-			#wget -O /home/volumio/logitechmediaserver/logitechmediaserver_7.9.2._all.deb http://downloads.slimdevices.com/nightly/7.9/sc/035da986803908908cb46501efbca6c4d6e93db2/logitechmediaserver_7.9.2~1543258616_all.deb
+			ARCH_DOWNLOAD="arm"
+			FILE=$(curl -s $BASE_URL$LATEST | grep $ARCH_DOWNLOAD | grep deb | cut -d'"' -f2)
+			wget -O /home/volumio/logitechmediaserver/logitechmediaserver_arm.deb $BASE_URL$LATEST$FILE
+		elif [ $cpu = "i686" ] || [ $cpu = "x86_64" ]; then
+			ARCH_DOWNLOAD="arm"
+			FILE=$(curl -s $BASE_URL$LATEST | grep $ARCH_DOWNLOAD | grep deb | cut -d'"' -f2)
+			wget -O /home/volumio/logitechmediaserver/logitechmediaserver.deb $BASE_URL$LATEST$FILE
 		fi
 		
 		# Move the binary to the expected directory
@@ -45,24 +51,6 @@ if [ ! -f $INSTALLING ]; then
 		apt-get install libio-socket-ssl-perl lame unzip -y
 		apt-get -f install -y
 		
-		# Get compiled CPAN for current Perl version and link it; if it doesn't exist
-		PERLV=$(perl -v | grep -o "(v[0-9]\.[0-9]\+" | sed "s/(v//;s/)//")
-		var=$(awk 'BEGIN{ print "'$PERLV'"<"'5.20'" }')
-		if [ "$var" -eq 0 -a ! -e /usr/share/squeezeboxserver/CPAN/arch/$PERLV/arm-linux-gnueabihf-thread-multi-64int/ ]; then
-			# get CPAN if not existing
-			if [ ! -e /opt/CPAN/$PERLV/arm-linux-gnueabihf-thread-multi-64int/ ]; then
-				wget -O /home/volumio/logitechmediaserver/CPAN_PERL_ALL.tar.gz https://github.com/Saiyato/volumio-lms-plugin/raw/master/known_working_versions/CPAN_PERL_ALL.tar.gz
-				tar -xvzf /home/volumio/logitechmediaserver/CPAN_PERL_ALL.tar.gz -C /opt/
-				echo "Download CPAN for Perl $PERLV"
-			fi
-			ln -sf /opt/CPAN/$PERLV/arm-linux-gnueabihf-thread-multi-64int/ /usr/share/squeezeboxserver/CPAN/arch/$PERLV/arm-linux-gnueabihf-thread-multi-64int
-			echo "Linking CPAN to Perl $PERLV"
-			sleep 4
-		else
-			ln -sf /opt/CPAN/arm-linux-gnueabihf-thread-multi-64int/ /usr/share/squeezeboxserver/CPAN/arch/5.18/
-			echo "Linking CPAN to Latest"
-		fi
-		
 		# These directories still use the old name; probably legacy code
 		echo "Fixing directory rights"
 		mkdir /var/lib/squeezeboxserver
@@ -72,27 +60,9 @@ if [ ! -f $INSTALLING ]; then
 		usermod -aG audio squeezeboxserver
 
 		# Add the systemd unit
-		echo "Adding the systemd unit"
-		rm /etc/systemd/system/logitechmediaserver.service	
-		wget -O /etc/systemd/system/logitechmediaserver.service https://raw.githubusercontent.com/Saiyato/volumio-lms-plugin/master/unit/logitechmediaserver.service
-		
-		# Image::Scale fix
-		echo "Fixing CPAN Image::Scale..."
-		wget -O /opt/CPAN_FIX_IMAGE.zip https://github.com/Saiyato/volumio-lms-plugin/raw/master/known_working_versions/CPAN_FIX_IMAGE.zip
-		unzip -o /opt/CPAN_FIX_IMAGE.zip -d /opt/CPAN/arm-linux-gnueabihf-thread-multi-64int
-		chmod 774 /opt/CPAN/arm-linux-gnueabihf-thread-multi-64int/auto/Image/Scale/Scale.so
-		echo "CPAN image fix completed"	
-		
-		# Fix Ubuntu interpreter
-		ln /lib/arm-linux-gnueabihf/ld-linux.so.3 /lib/ld-linux.so.3
-		
-		# Audio fix for DSD
-		echo "Fixing CPAN DSD playback..."
-		wget -O /opt/CPAN_AUDIO_DSD_7.9.tar https://github.com/Saiyato/volumio-lms-plugin/raw/master/known_working_versions/CPAN_AUDIO_DSD_7.9.tar
-		tar -xf /opt/CPAN_AUDIO_DSD_7.9.tar -C /opt
-		wget -O /opt/DSDPLAYER-BIN.zip https://github.com/Saiyato/volumio-lms-plugin/raw/master/known_working_versions/DSDPLAYER-BIN.zip
-		unzip -o /opt/DSDPLAYER-BIN.zip -d /usr/share/squeezeboxserver/Bin/
-		echo "CPAN DSD playback fix completed"
+		echo "Using the prepared systemd unit"
+		rm /etc/systemd/system/logitechmediaserver.service
+		ln -fs /data/plugins/music_service/lms/unit/logitechmediaserver.service /etc/systemd/system/logitechmediaserver.service
 		
 		# Stop service and fix rights for preference folder
 		service logitechmediaserver stop
@@ -102,9 +72,6 @@ if [ ! -f $INSTALLING ]; then
 		
 		# Tidy up
 		rm -rf /home/volumio/logitechmediaserver
-		rm /opt/CPAN_AUDIO_DSD_7.9.tar
-		rm /opt/CPAN_FIX_IMAGE.zip
-		rm /opt/DSDPLAYER-BIN.zip
 		
 		# Reload the systemd unit
 		systemctl daemon-reload
